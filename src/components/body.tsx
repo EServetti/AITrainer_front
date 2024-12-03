1af9979232eda0bb3c2151e8961def4198506d04
unknown <eservetti2018@gmail.com>
2024-12-03 02:18:56 -0300

    Agregada pagina about

--- src/components/body.tsx
+++ src/components/body.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom"
 import Home from "../pages/home"
 import "../style/main.css"
+import About from "../pages/about"
 
 function Main() {
     return (
         <div className="main">
         <Routes>
             <Route path="/" Component={Home}/>
+            <Route path="/about" Component={About}/>
         </Routes>
         </div>
     )
